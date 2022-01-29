44e7fc7a0a16248aa4f6a7b8868169d320c8c618
Lee Macri <lem@rathkeale.school.nz>
2022-01-30 09:58:20 +1300

    Update install-tkp.sh
    Added code to set eclipse workspace to tkp-kasm-java dir.

--- src/ubuntu/install/tkp/install_tkp.sh
+++ src/ubuntu/install/tkp/install_tkp.sh
@@ -2,11 +2,16 @@
 mkdir ~/TKPSource
 cd ~/TKPSource
 
+# Download adapted TKP
 wget -O master.zip https://github.com/rathkealedgt/tkpkasmjava/archive/refs/heads/main.zip
 unzip master.zip
 rm master.zip
 
+# Set up workspace
 mv tkpkasmjava-main tkp-kasm-java
 cd tkp-kasm-java
-
 unzip eclipse_workspace.zip
+
+# Set eclipse desktop icon to open TKP
+echo Exec=/opt/eclipse/eclipse -data $HOME/TKPSource/tkp-kasm-java >>~/Desktop/eclipse.desktop
+chmod +x ~/Desktop/eclipse.desktop
